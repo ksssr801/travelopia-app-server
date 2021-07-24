dfeafab74ab382699f7d34543e235d6d4367dba6
krsahas801 <krsahas.git@gmail.com>
2021-07-25 01:18:36 +0530

    backend APIs completed

--- src/travel_app/models.py
+++ src/travel_app/models.py
@@ -4,9 +4,12 @@ class BookingDetails(models.Model):
     booking_id = models.IntegerField(primary_key=True)
     traveller = models.CharField(max_length=254)
     email_id = models.EmailField(max_length=254)
-    destination = models.IntegerField()
-    total_travellers = models.IntegerField()
-    budget_per_person = models.IntegerField()
+    destination = models.IntegerField(null=True)
+    total_travellers = models.IntegerField(null=True)
+    budget_per_person = models.IntegerField(null=True)
+    last_update_time = models.IntegerField(null=True)
+    creation_time = models.IntegerField(null=True)
+    is_deleted = models.BooleanField(default=False)
 
     class Meta:
         db_table = 'tbl_booking_details'
--- src/travel_app/views.py
+++ src/travel_app/views.py
@@ -1,8 +1,10 @@
 from rest_framework import status, viewsets
 from rest_framework.response import Response
+from rest_framework.decorators import action
 from .serializers import BookingDetailsSerializers
 from .models import BookingDetails
-from rest_framework.decorators import action
+from .utils.utility import *
+from .utils.default_config import *
 
 class TravelAppViewSet(viewsets.ModelViewSet):
     serializer_class = BookingDetailsSerializers
@@ -12,13 +14,22 @@ class TravelAppViewSet(viewsets.ModelViewSet):
 
     @action(detail=False, methods=['post'], name="do_booking", url_path='do-booking')
     def get_booking_info(self, request):
-        print ("request : ", request.data)
-        return Response({})
+        try:
+            print ("request : ", request.data)
+            data_obj = request.data
+            
+            return Response({}, status=status.HTTP_200_OK)
+        except Exception as err:
+            return Response({}, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
 
     @action(detail=False, methods=['get'], name="booking_details", url_path='booking-details')
     def get_booking_details(self, request):
-        print ("request : ", request.GET)
-        all_booking_details = BookingDetails.objects.all()
-        serialize_data = BookingDetailsSerializers(all_booking_details, many=True).data
-        print ("serialize_data : ", serialize_data)
-        return Response(serialize_data, status=status.HTTP_200_OK)
+        try:
+            print ("request : ", request.GET, get_ID(), DESTINATION_LIST)
+            all_booking_details = BookingDetails.objects.all()
+            serialize_data = BookingDetailsSerializers(all_booking_details, many=True).data
+            print ("serialize_data : ", serialize_data)
+            return Response(serialize_data, status=status.HTTP_200_OK)
+        except Exception as err:
+            return Response(serialize_data, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
+    
